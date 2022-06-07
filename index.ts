20e5c9358fa9fccbbe8921b30b60a4a1b96453e6
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-06-07 11:07:14 +0300

    Port fix

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ export const PROJECT_DIRECTION = __dirname
 
 async function startServer() {
     try {
-        app.listen(process.env.YAZMEYAA_BACKEND_PORT || config.get('PORT'), () => console.log(`Server started at port ${config.get('PORT')}`))
+        app.listen(process.env.PORT || config.get('PORT'), () => console.log(`Server started at port ${config.get('PORT')}`))
 
         mongoose.connect(config.get('mongoDBsecret'), (error) => {
             if (error) {
